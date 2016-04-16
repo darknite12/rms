25fe6fb9dd8f3234f37c48ef5a05b4d7f59aa665
Francisco Lara <flara.commercial@gmail.com>
2016-04-16 13:54:23 -0400

    created some methods for Table Repository class

--- maven/rms-jpa/src/main/java/org/rms/repository/TableRepository.java
+++ maven/rms-jpa/src/main/java/org/rms/repository/TableRepository.java
@@ -1,10 +1,16 @@
 package org.rms.repository;
 
+import java.util.List;
+
 import org.rms.model.Table;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TableRepository extends CrudRepository<Table, Integer> {
 	
+	Table findTableByTableNameAndYear(String tableName, int year);
 	
+	Table findTableByTableNumberAndYear(int tableNumber, int year);
+	
+	List<Table> findTablesByYear(int year);
 
 }
