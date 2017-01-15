7ac01ea35d0eedb75dee172e30cc09d90ef3eb02
ubuntu <ubuntu@ubuntu>
2017-01-15 20:30:57 +0000

    First attempt at first controller.

---
+++ maven/rms-web/src/main/java/org/rms/web/controllers/constants/URLConstants.java
@@ -0,0 +1,6 @@
+package org.rms.web.controllers.constants;
+
+public class URLConstants {
+	
+	public static final String API = "/api";
+}
---
+++ maven/rms-web/src/main/java/org/rms/web/main/RMSApplication.java
@@ -0,0 +1,13 @@
+package org.rms.web.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RMSApplication.class, args);
+    }
+
+}
