e960563cb6dad6aaf926a9371d0b17a928d6b4b6
Francisco Lara <Francisco Lara>
2017-03-26 02:41:13 -0400

    add search parameter

--- maven/rms-donation-manager/src/main/java/org/rms/domain/PersonRepository.java
+++ maven/rms-donation-manager/src/main/java/org/rms/domain/PersonRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.rms.domain.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 	
-	List<Person> findByFirstName(String firstName);
+	List<Person> findByFirstName(@Param("firstName") String firstName);
 	
 	
 }
