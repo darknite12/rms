c06189aa219f56afc855c81e512864364edd3508
Edson <Edson@DESKTOP-DGOODCJ>
2017-08-02 12:06:13 -0500

    Controllers for each view

---
+++ maven/rms-donation-manager/src/main/java/org/rms/web/CreatePersonController.js
@@ -0,0 +1,6 @@
+/**
+ * This is the controller for the createPerson template
+ */
+mainApp.controller('CreatePersonController', function($scope) {
+	$scope.message = "This page will be use to display createPerson form";
+});
---
+++ maven/rms-donation-manager/src/main/java/org/rms/web/RemovePersonController.js
@@ -0,0 +1,6 @@
+/**
+ * This is the controller for the removePerson template
+ */
+mainApp.controller('RemovePersonController', function($scope) {
+	$scope.message = "This page will be use to display removePerson form";
+});
