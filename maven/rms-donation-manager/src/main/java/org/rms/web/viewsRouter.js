0e644f83ddbc033f84f5abebe816ab96cbac199d
Edson <Edson@DESKTOP-DGOODCJ>
2017-08-04 11:16:03 -0500

    Trying to fix the problem

--- maven/rms-donation-manager/src/main/java/org/rms/web/viewsRouter.js
+++ maven/rms-donation-manager/src/main/java/org/rms/web/viewsRouter.js
@@ -2,29 +2,26 @@
  * This file defines the configuration of how the different views (forms) are shown
  */
 var mainApp = angular.module("mainApp", ['ngRoute']);
-
-mainApp.config(['$routeProvider',
-	/*This function shows a view and matches it with its respective controller*/
-	function($routeProvider) {
-		$routeProvider.
-		when('/viewPerson', {
-			templateUrl: 'viewPerson.htlm',
-			controller: 'ViewPersonControler'
-		}).
-		when('/createPerson', {
-			templateUrl: 'createPerson.htlm',
-			controller: 'CreatePersonControler'
-		}).
-		when('/modifyPerson', {
-			templateUrl: 'modifyPerson.htlm',
-			controller: 'ModifyPersonControler'
-		}).
-		when('/removePerson', {
-			templateUrl: 'removePerson.htlm',
-			controller: 'RemovePersonControler'
-		}).
-		otherwise({
-			redirectTo: 'viewPerson'/*The reference to the main or default view  goes here*/
-			});
-	}
-]);
+/*This function shows a view and matches it with its respective controller*/
+mainApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+	when('/viewPerson', {
+		templateUrl: 'viewPerson.htlm',
+		/*controller: 'ViewPersonControler'*/
+	}).
+	when('/createPerson', {
+		templateUrl: 'createPerson.htlm',
+		/*controller: 'CreatePersonControler'*/
+	}).
+	when('/modifyPerson', {
+		templateUrl: 'modifyPerson.htlm',
+		/*controller: 'ModifyPersonControler'*/
+	}).
+	when('/removePerson', {
+		templateUrl: 'removePerson.htlm',
+		/*controller: 'RemovePersonControler'*/
+	}).
+	otherwise({
+		redirectTo: 'viewPerson'/*The reference to the main or default view  goes here*/
+	});
+}]);
