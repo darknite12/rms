12c04b4609d24f55f8acfde5d1774cf047e1e604
Franisco <flara.commercial@gmail.com>
2018-01-27 15:59:46 -0500

    #11 - added 'search by search parameter' to person entity

--- rms-donation-manager/src/main/java/org/rms/domain/PersonRepository.java
+++ rms-donation-manager/src/main/java/org/rms/domain/PersonRepository.java
@@ -2,6 +2,9 @@ package org.rms.domain;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -18,5 +21,10 @@ public interface PersonRepository extends PagingAndSortingRepository<Person, Int
 	List<Person> findByCellPhoneNumber(@Param("cellPhoneNumber") String cellPhoneNumber);
 
 	List<Person> findByHomePhoneNumber(@Param("homePhoneNumber") String homePhoneNumber);
+
+	@Query("select p from Person p where p.firstName like %:searchParameter% or p.lastName like %:searchParameter% or "
+			+ "p.parish like %:searchParameter% or p.language like %:searchParameter% or p.workPhoneNumber like %:searchParameter% or "
+			+ "p.cellPhoneNumber like %:searchParameter% or p.homePhoneNumber like %:searchParameter% or p.email like %:searchParameter% ")
+	Page<Person> findBySearchString(@Param("searchParameter") String searchParameter, Pageable page);
 
 }
