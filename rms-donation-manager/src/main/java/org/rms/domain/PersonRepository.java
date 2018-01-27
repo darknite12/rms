51d887dc9ef7e5596961b9492bfb60073cac865c
Franisco <flara.commercial@gmail.com>
2018-01-27 16:00:52 -0500

    #11 - removed unnecessary search services in person entity

--- rms-donation-manager/src/main/java/org/rms/domain/PersonRepository.java
+++ rms-donation-manager/src/main/java/org/rms/domain/PersonRepository.java
@@ -1,7 +1,5 @@
 package org.rms.domain;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -9,18 +7,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface PersonRepository extends PagingAndSortingRepository<Person, Integer> {
-
-	List<Person> findByFirstName(@Param("firstName") String firstName);
-
-	List<Person> findByLastName(@Param("lastName") String lastName);
-
-	List<Person> findByFirstNameAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
-
-	List<Person> findByEmail(@Param("email") String email);
-
-	List<Person> findByCellPhoneNumber(@Param("cellPhoneNumber") String cellPhoneNumber);
-
-	List<Person> findByHomePhoneNumber(@Param("homePhoneNumber") String homePhoneNumber);
 
 	@Query("select p from Person p where p.firstName like %:searchParameter% or p.lastName like %:searchParameter% or "
 			+ "p.parish like %:searchParameter% or p.language like %:searchParameter% or p.workPhoneNumber like %:searchParameter% or "
