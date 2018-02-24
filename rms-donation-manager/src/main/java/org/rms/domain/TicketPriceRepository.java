9c12afd0eabb382b094b09bcc610631d56e4decf
Edson Andree <edsonandree@gmail.com>
2018-02-23 21:42:00 -0500

    #16 Search for ticketPrice table
    A search option was added. Now a price can be found by year

--- rms-donation-manager/src/main/java/org/rms/domain/TicketPriceRepository.java
+++ rms-donation-manager/src/main/java/org/rms/domain/TicketPriceRepository.java
@@ -1,7 +1,10 @@
 package org.rms.domain;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TicketPriceRepository extends PagingAndSortingRepository<TicketPrice, Integer> {
+	
+	TicketPrice findByYear(@Param("year") int year);
 
 }
