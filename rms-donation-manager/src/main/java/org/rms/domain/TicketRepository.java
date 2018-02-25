f6ca1aa5c4badaff7836472a4a1d368fa0856cb4
Edson Andree <edsonandree@gmail.com>
2018-02-25 12:23:00 -0500

    Revert "#16 Searching Tickets"
    This reverts commit ef792288cc427d9be2731fc5f4ad7b4b117216d3.

--- rms-donation-manager/src/main/java/org/rms/domain/TicketRepository.java
+++ rms-donation-manager/src/main/java/org/rms/domain/TicketRepository.java
@@ -1,13 +1,9 @@
 package org.rms.domain;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface TicketRepository extends PagingAndSortingRepository<Ticket, Integer> {
 
-	Ticket findByTicketNumber(@Param("ticketNumber") String ticketNumber);
-	Page<Ticket> findByYear(@Param("year") Integer year, Pageable page);
+	Ticket findByTicketNumberAndYear(String ticketNumber, Integer year);
 
 }
