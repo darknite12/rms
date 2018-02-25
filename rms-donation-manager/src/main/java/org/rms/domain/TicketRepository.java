ef792288cc427d9be2731fc5f4ad7b4b117216d3
Edson Andree <edsonandree@gmail.com>
2018-02-25 12:20:24 -0500

    #16 Searching Tickets
    The options to search tickets, by number and year, have been added

--- rms-donation-manager/src/main/java/org/rms/domain/TicketRepository.java
+++ rms-donation-manager/src/main/java/org/rms/domain/TicketRepository.java
@@ -1,9 +1,13 @@
 package org.rms.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TicketRepository extends PagingAndSortingRepository<Ticket, Integer> {
 
-	Ticket findByTicketNumberAndYear(String ticketNumber, Integer year);
+	Ticket findByTicketNumber(@Param("ticketNumber") String ticketNumber);
+	Page<Ticket> findByYear(@Param("year") Integer year, Pageable page);
 
 }
