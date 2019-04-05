d7908a649c0e4048adde38c7a6e9aa0b7fc590ab
Franisco <flara.commercial@gmail.com>
2019-04-04 22:36:31 -0400

    #53 - adding findByIsActiveTrue method

--- rms-donation-manager/src/main/java/org/rms/domain/EventRepository.java
+++ rms-donation-manager/src/main/java/org/rms/domain/EventRepository.java
@@ -1,7 +1,11 @@
 package org.rms.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Integer> {
+
+	List<Event> findByIsActiveTrue();
 
 }
